2c467ba571f1309bcedcdbc3731bf5b5baf6320d
TytanF <103470320+TytanF@users.noreply.github.com>
2022-04-12 22:12:56 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'ecliptics'
+description 'PiggyBack'
+version '1.0.0'
+
+client_script "cl_piggyback.lua"
+server_script "sv_piggyback.lua"
